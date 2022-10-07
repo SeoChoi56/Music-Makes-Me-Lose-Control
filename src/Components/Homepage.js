5f6af11714e1a3dd737d0a338b941523da10b5e2
Ismael Aceves <ismael@Ismaels-MacBook-Pro.local>
2022-10-07 13:23:42 -0400

    start css

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -32,29 +32,39 @@ function Homepage({userDetail, setProfile }){
     const favoritesList = userDetail.playlist.map((song) => {
 
         return (
-            <div key={song.id} id={song.id}>{song.title} by {song.artist + " "} 
-            <img className="albumCovers" src={song.albumCover} />
-                <button onClick={handleDeleteSong}> Delete</button>
-            </div>)
+            <div className="song">
+                <div className="song-body"  key={song.id} id={song.id}>
+                    <h4> {song.title} by {song.artist + " "} </h4>
+                    <img className="albumCovers" src={song.albumCover} />
+                    <button onClick={handleDeleteSong}> Delete</button>
+                </div>
+            </div>
+            )
     })
 
 
 
     return (
-        <div id="homePage">
-            <span>
-                This is User Profile: {userDetail.username}
-                <div>
-                    <img src={userDetail.avatarURL} />
+        
+        <> 
+                <div className="user-profile">
+                    This is User Profile: {userDetail.username}
+                    <div className="">
+                        <img className="userPhoto" src={userDetail.avatarURL} />
+                    </div>
+                    <div>Favorite Artist: {userDetail.artist}</div>
+                    <div>Favorite Genre: {userDetail.genre}</div>
                 </div>
-                <div>Favorite Artist: {userDetail.artist}</div>
-                <div>Favorite Genre: {userDetail.genre}</div>
-            </span>
-            <span>
-                This is User Playlist: {favoritesList} 
-                
-            </span>
-        </div>
+
+                <div className="userPlaylistCard">
+                    <h3>This is User Playlist:</h3> 
+                    <div className='fav-list'>
+                        {favoritesList}
+                    </div> 
+            
+                </div>
+        </>
+    
     )
 }
 
